2f42281835972b4419932d48a393330c39ff92b9
Felix Leupold <felix@xiel.de>
2019-10-04 10:09:17 +0200

    remove old graph component, extract header (#3)
    Signed-off-by: Felix Leupold <felix@xiel.de>

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,22 +1,14 @@
 import React from 'react'
-import Graph from './components/Graph/Graph'
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import SimpleWheelDrag from './components/SimpleWheelDrag/SimpleWheelDrag'
+import Header from './components/Header/Header'
 
 const App: React.FC = () => {
   return (
     <Router>
-      <h1>wheelAnalyzer</h1>
-      <nav style={{ display: 'inline-grid', gridAutoFlow: 'column', gridGap: '1em', margin: '1em 0' }}>
-        <Link to="/">Home</Link>
-        <Link to="/graph">Graph</Link>
-        <Link to="/simple-wheel-drag">SimpleWheelDrag</Link>
-      </nav>
-
+      <Header />
       <Switch>
         <Route exact path="/" component={SimpleWheelDrag} />
-        <Route path="/graph" component={Graph} />
-        <Route path="/simple-wheel-drag" component={SimpleWheelDrag} />
       </Switch>
     </Router>
   )
