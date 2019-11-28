ad3177b3055b0d04bbef17ceb80a97e7c77a51b3
Felix Leupold <felix@xiel.de>
2019-11-28 20:41:13 +0100

    snap to closestSnapPoint

--- examples/src/components/Gallery/Gallery.tsx
+++ examples/src/components/Gallery/Gallery.tsx
@@ -15,35 +15,57 @@ const pages = [
   'https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=1&h=600&w=1000',
 ]
 
-const DECELERATION_RATE = 0.998
+const DECELERATION_RATE = 0.995
 const projection = (startVelocityPxMs: number) => (startVelocityPxMs * DECELERATION_RATE) / (1 - DECELERATION_RATE)
 
+// TODO: add select box to toggle momentumDetection
 export default function Gallery() {
   const containerRef = useRef<HTMLDivElement | null>(null)
   const offsetX = useRef(0)
   const [spring, set] = useSpring(() => ({
     x: 0,
-    config: springCfg.slow,
+    config: springCfg.stiff,
   }))
 
   useWheelDrag(
     ({ down, delta: [x], axisVelocity }) => {
       const [xVelo] = axisVelocity
-      let projectionX = projection(xVelo) * -1
-
-      console.log(x, xVelo)
 
       if (down) {
         set({ x: down ? offsetX.current + x : 0, immediate: down })
       } else {
-        // projectionX = projectionX + (projectionX + offsetX.current) % window.innerWidth
+        const width = window.innerWidth
         const config = { velocity: xVelo * -1 }
-        // const config = undefined
-        offsetX.current += x
-        set({ x: offsetX.current + projectionX, config })
-        offsetX.current += projectionX
-      }
+        const snapPoints = pages.map((_, i) => i * width * -1)
 
+        console.log(x, xVelo)
+        const projectionX = projection(xVelo) * -1
+        const projectedTarget = offsetX.current + x + projectionX
+
+        console.log('width', width)
+        console.log('snapPoints', snapPoints)
+        console.log('projectionX', projectionX)
+        console.log('projectedTarget', projectedTarget)
+
+        const closestSnapPoint = snapPoints.reduce(
+          (acc, snapPoint) => {
+            const diff = snapPoint - acc.projectedTarget
+            const isCloser = Math.abs(diff) < Math.abs(acc.closestDiff)
+            return isCloser ? { ...acc, closest: snapPoint, closestDiff: diff } : acc
+          },
+          {
+            projectedTarget: projectedTarget,
+            closest: Infinity,
+            closestDiff: Infinity,
+          }
+        )
+
+        console.log('closestSnapPoint', closestSnapPoint)
+
+        offsetX.current = closestSnapPoint.closest
+
+        set({ x: closestSnapPoint.closest, config, onFrame: (currentValue) => (offsetX.current = currentValue.x || 0) })
+      }
     },
     { domTarget: containerRef, axis: 'x', wheelReason: WheelReason.USER }
   )
