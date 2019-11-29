9ad2985deb8b06062c98b2176d52b74c6d847b9e
Felix Leupold <felix@xiel.de>
2019-11-29 09:25:12 +0100

    add select for wheel reason

--- examples/src/components/Gallery/Gallery.tsx
+++ examples/src/components/Gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { animated, config as springCfg, useSpring } from 'react-spring'
 
 import c from './Gallery.module.scss'
@@ -15,39 +15,49 @@ const pages = [
   'https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=1&h=600&w=1000',
 ]
 
-const DECELERATION_RATE = 0.995
-const projection = (startVelocityPxMs: number) => (startVelocityPxMs * DECELERATION_RATE) / (1 - DECELERATION_RATE)
+const DECAY_FAST = 0.99
+const DECAY_NORMAL = 0.998
+const projection = (startVelocityPxMs: number) => (startVelocityPxMs * DECAY_FAST) / (1 - DECAY_FAST)
+
+const rubberband = (distance: number, dimension: number, constant = 0.55) =>
+  (distance * dimension * constant) / (constant * distance + dimension)
+
+const rubberbandClamp = (min: number, max: number, delta: number, constant?: number) =>
+  delta < min
+    ? -rubberband(min - delta, max - min, constant) + min
+    : delta > max
+    ? rubberband(delta - max, max - min, constant) + max
+    : delta
 
 // TODO: add select box to toggle momentumDetection
 export default function Gallery() {
   const containerRef = useRef<HTMLDivElement | null>(null)
   const offsetX = useRef(0)
+  const [selectedWheelReason, setSelectedWheelReason] = useState(WheelReason.USER)
   const [spring, set] = useSpring(() => ({
-    x: 0,
+    x: offsetX.current,
     config: springCfg.stiff,
   }))
 
-  const onFrame = (currentValue: { x?: number }) => (offsetX.current = (currentValue.x ?? offsetX.current))
+  const onFrame = (currentValue: { x?: number }) => {
+    offsetX.current = currentValue.x ?? offsetX.current
+  }
 
   useWheelDrag(
     ({ down, delta: [x], axisVelocity }) => {
       const [xVelo] = axisVelocity
 
       if (down) {
-        set({ x: down ? offsetX.current + x : 0, immediate: down })
+        const unrubberedX = offsetX.current + x
+        // const rubberedX = rubberbandClamp(-2000, 0, unrubberedX)
+
+        set({ x: down ? unrubberedX : 0, immediate: down })
       } else {
         const width = window.innerWidth
         const config = { velocity: xVelo * -1 }
         const snapPoints = pages.map((_, i) => i * width * -1)
-
-        console.log(x, xVelo)
         const projectionX = projection(xVelo) * -1
         const projectedTarget = offsetX.current + x + projectionX
-
-        console.log('width', width)
-        console.log('snapPoints', snapPoints)
-        console.log('projectionX', projectionX)
-        console.log('projectedTarget', projectedTarget)
 
         const closestSnapPoint = snapPoints.reduce(
           (acc, snapPoint) => {
@@ -62,18 +72,19 @@ export default function Gallery() {
           }
         )
 
-        console.log('closestSnapPoint', closestSnapPoint)
-
         offsetX.current = closestSnapPoint.closest
-
         set({ x: closestSnapPoint.closest, config, onFrame })
       }
     },
-    { domTarget: containerRef, axis: 'x', wheelReason: WheelReason.USER }
+    { domTarget: containerRef, axis: 'x', wheelReason: selectedWheelReason }
   )
 
   return (
     <div ref={containerRef} className={c.gallery}>
+      <select value={selectedWheelReason} onChange={(e) => setSelectedWheelReason(e.target.value as WheelReason)}>
+        <option value={WheelReason.USER}>momentum detection</option>
+        <option value={WheelReason.ANY}>no momentum detection</option>
+      </select>
       <animated.div style={{ x: spring.x }}>
         {pages.map((url, i) => (
           <div key={i}>
