a8babf6ea560a147f01fc601257234613460a9bb
Felix Leupold <felix@xiel.de>
2020-04-18 12:41:35 +0200

    extract state, add average helper, manual PhaseData type

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,3 +5,7 @@ export function lastOf<T>(array: T[]) {
 export function firstOf<T>(array: T[]) {
   return array[0]
 }
+
+export function average(numbers: number[]) {
+  return numbers.reduce((a, b) => a + b) / numbers.length
+}
